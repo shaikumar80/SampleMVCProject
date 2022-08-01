c493c63f0dc444969f7ff7b2f418d080fe50c20a
Umar <Umar@gmail.com>
2022-08-01 09:17:38 +0530

    First commit for SampleMvcProject

---
+++ SampleMvcProject/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMvcProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string PostUsingParameter(string firstname, string lastname, string email, string address, string address2, string city, string zip)
+        {
+            string details = "User Registered Successfully!! You details are First Name: " + firstname + ", Last Name: " + lastname + ", Email: " + email + ", Address: " + address + ", Address2: " + address2 + ", City: " + city + ", Zip: " + zip; 
+            return details;
+        }
+    }
+}
---
+++ SampleMvcProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleMvcProject.MvcApplication" Language="C#" %>
